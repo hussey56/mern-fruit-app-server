a5bbdeaddedfd2f1e9f194fde42fd7498837a54f
hussey56 <hassankhan032370@gmail.com>
2023-01-25 00:56:38 +0500

    c

--- middleware/imageMiddleWare.js
+++ middleware/imageMiddleWare.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './server/uploads')
+      cb(null, './uploads')
     },
     filename: function (req, file, cb) {
      const uniqueName = `${Date.now()}+${file.originalname}`;
